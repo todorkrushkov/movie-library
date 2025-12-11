37faacca87b4e4e2519d6608310ff13dcbe71d9f
Todor Krushkov <todorkrushkov.1304@gmail.com>
2025-12-11 10:24:17 +0200

    implement Movie

--- src/main/java/telerik/project/movielibrary/models/Movie.java
+++ src/main/java/telerik/project/movielibrary/models/Movie.java
@@ -1,5 +1,36 @@
 package telerik.project.movielibrary.models;
 
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "movies")
+
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Movie {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    @Column(name = "movie_id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "director")
+    private String director;
+
+    @Column(name = "release_year")
+    private Integer releaseYear;
+
+    @Column(name = "rating")
+    private Double rating;
+
 }
