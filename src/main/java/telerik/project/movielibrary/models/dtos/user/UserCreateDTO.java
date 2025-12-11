2929900588b6453fb8fe02df04deb7dce721b12d
Todor Krushkov <todorkrushkov.1304@gmail.com>
2025-12-12 01:01:33 +0200

    implement DTOs
    also create mappers & implement them in controllers

---
+++ src/main/java/telerik/project/movielibrary/models/dtos/user/UserCreateDTO.java
@@ -0,0 +1,22 @@
+package telerik.project.movielibrary.models.dtos.user;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserCreateDTO {
+
+    @NotBlank
+    @Size(min = 3, max = 32)
+    private String username;
+
+    @NotBlank
+    @Size(max = 128)
+    private String password;
+
+}
